8baadc4484fbf0e53975d45f3f34637be7bc1848
MinsooKang <minsu1599@naver.com>
2025-11-13 14:13:47 +0900

    배포 우선

--- src/test/java/com/privacy/privacyplatform/PrivacyPlatformApplicationTests.java
+++ src/test/java/com/privacy/privacyplatform/PrivacyPlatformApplicationTests.java
@@ -1,38 +1,21 @@
 package com.privacy.privacyplatform;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.ActiveProfiles;
-import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration"
+})
 class PrivacyPlatformApplicationTests {
-
-    @TestConfiguration
-    static class TestConfig {
-
-        @Bean
-        @Primary
-        public JavaMailSender javaMailSender() {
-            return Mockito.mock(JavaMailSender.class);
-        }
-
-        @Bean
-        @Primary
-        public S3Client s3Client() {
-            return Mockito.mock(S3Client.class);
-        }
-    }
 
     @Test
     void contextLoads() {
-        // Spring Context 로딩 테스트
+        // 최소한의 컨텍스트만 로드하여 테스트
     }
 
 }
